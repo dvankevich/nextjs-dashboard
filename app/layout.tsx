ebc4a47f091ca2533b90d2b57ddc9ba0a3a5a346
dvankevich <dvankevich@gmail.com>
2025-12-29 20:04:00 +0200

    add acme-logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,20 @@
 import "@/app/ui/global.css";
-import { Roboto } from "next/font/google";
+//import { Roboto } from "next/font/google";
+import { inter } from "@/app/ui/fonts";
 
-const roboto = Roboto({
-  weight: "400",
-  subsets: ["latin"],
-});
+// const roboto = Roboto({
+//     weight: "400",
+//     subsets: ["latin"],
+// });
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className={roboto.className}>
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${inter.className} antialiased`}>{children}</body>
+        </html>
+    );
 }
